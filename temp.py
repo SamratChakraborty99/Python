400ed62c3ae28e3dc3ec1abb613cb1646bb99405
Samrat Chakraborty <57918935+SamratChakraborty99@users.noreply.github.com>
2023-08-24 14:27:33 +0530

    Add files via upload
    First lab Multivariant

---
+++ temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import pandas as pd
+df = pd.read_csv("C:/Users/DELL/Desktop/IIT DHANBAD LAB/exp1.csv")
+df.head()
+df.describe()
+
+
